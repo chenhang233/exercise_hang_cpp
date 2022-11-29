f4961e375f62c7e015df548ef446e472799e9c03
chenhang233 <1973433180@qq.com>
2022-11-29 17:41:53 +0800

    结构体案例练习

--- 14.结构体.cpp
+++ 14.结构体.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
 using namespace std;
 
-typedef struct Student {
+typedef struct student {
 	string name;
 	int age;
 	float score;
 } Student;
 
-int main() {
+void printStu1(Student s) {
+	cout << s.name << endl;
+}
+
+void printStu2(const Student *p) {
+	//p->age = 500;
+}
+
+
+int main14() {
 	Student s1 = { "zs", 50, 6.1 };
-	Student s2;
-	s2.score = 5.64;
-	s2.age = 40;
-	cout << s1.name << s1.age << s1.score << endl;
+	Student s2[5] = {
+		{"a", 4,54},
+		{"b", 54, 8}
+	};
+	printStu1(s1);
+	printStu2(&s1);
+	Student* p = &s1;
+	cout << s1.name << s1.age << "\n   ----name age-----" << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << s2[i].name << s2[i].age << s2[i].score << '\n' << endl;
+	}
+
+	cout << p->age << '\n' << endl;
+
 	system("pause");
 	return 0;
 }
---
+++ 15结构体案例1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std;
+
+struct Stu {
+	string name;
+	int score;
+};
+
+ struct Tea {
+	string name;
+	Stu stuArr[3];
+};
+
+void init(struct Tea tArr[]) {
+	string nameMap = "ABCD";
+	for (int i = 0; i < 3; i++)
+	{
+		tArr[i].name = "老师";
+		tArr[i].name  += nameMap[i];
+		for (int j = 0; j < 3; j++)
+		{
+			tArr[i].stuArr[j].name = "学生";
+			tArr[i].stuArr[j].name += nameMap[j];
+			tArr[i].stuArr[j].score = rand() % 60 + 20;
+		}
+	}
+}
+
+
+void printInfo(struct Tea tArr[]) {
+	for (int i = 0; i < 3; i++)
+	{
+		cout << tArr[i].name << endl;
+	for (int j = 0; j < 3; j++)
+		{
+			cout << '\t' << tArr[i].stuArr[j].name << endl;
+			cout << "分数=" << tArr[i].stuArr[j].score << endl;
+
+		}
+	}
+}
+
+int main() {
+	srand((unsigned int)time(NULL));
+	struct Tea teaArr[3] = {};
+	init(teaArr);
+	printInfo(teaArr);
+	system("pause");
+	return 0;
+}
